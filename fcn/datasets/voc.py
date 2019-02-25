3a2204f48fe9c0f40f2bc928f3df1d1eb00505a5
dcooo <jinhaodcooo@gmail.com>
2019-02-25 10:35:45 +0800

    Initialize Repository

---
+++ fcn/datasets/voc.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from torch.utils import data
+
+class VOCClassSeg(data.Dataset):
+    
+    def __init__(self):
+        pass
+    
+    def __len__(self):
+        pass
+    
+    def __getitem__(self, item):
+        pass
---
+++ fcn/models/fcn32s.py
@@ -0,0 +1,18 @@
+
+import torch
+import torch.nn as nn
+import torch.functional as F
+
+
+class FCN32s(nn.Module()):
+    
+    def __init__(self):
+        super(FCN32s, self).__init__()
+    
+    
+    def forward(self, x):
+        pass
+    
+    
+    def _initialize_weights(self):
+        pass
